c020588939c2db9630ec655fd8fdbcd0d7d63ec6
Fangzheng Zhang <fz2907@vt.edu>
2023-03-30 17:27:19 -0400

    "TODO: complete cache feature"

--- Simple-Game-Server-Management-Tool/src/main/java/cat/frank/SimpleGameServerManagementTool/config/ConfigService.java
+++ Simple-Game-Server-Management-Tool/src/main/java/cat/frank/SimpleGameServerManagementTool/config/ConfigService.java
@@ -9,8 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Configuration;
 
+import cat.frank.SimpleGameServerManagementTool.serverIp.IPService;
 import cat.frank.SimpleGameServerManagementTool.utility.ApplicationShutdownManager;
 
 @Configuration
@@ -19,10 +22,16 @@ public class ConfigService {
     @Autowired
     ApplicationShutdownManager shutdownManager = new ApplicationShutdownManager();
 
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Autowired
+    IPService ipService = new IPService();
+
     //add logger
     private static final Logger logger = LoggerFactory.getLogger(ConfigService.class);
 
-    @Value("${sgsmt.root.path:}")
+    @Value("${sgsmt.root.path:../}")
     private String appRootPath;
 
     @Value("${sgsmt.scripts.path:scripts}")
@@ -36,7 +45,7 @@ public class ConfigService {
     public void initChecker(){
 
         if(appRootPath == null || appRootPath.isEmpty() ||appRootPath.equals("/")){
-            logger.error("sgsmt.root.path is null or empty. Please set it in the application.properties file.");
+            logger.error("sgsmt.root.path is null or empty. Please start the application with StartInit.sh or set the Application Home.");
             // Shut down the spring appliction
             shutdownManager.initiateShutdown(1);
         }
@@ -48,23 +57,37 @@ public class ConfigService {
         logger.info("infoFilePath: " + infoFilePath);
 
         checkIfPathExists();
+
+        Cache cache = cacheManager.getCache("PathCache");
+        cache.put("sgsmt.root.path", appRootPath);
+        cache.put("sgsmt.scripts.path", scriptsPath);
+        cache.put("sgsmt.info.file.path", infoFilePath);
+
+        checkIfIpChanged();
         
+    }
+
+    private void checkIfIpChanged() {
+        ipService.checkIfIpChanged();
     }
 
     /**
      * This function checks if the path exists and if not, if it is not exist, then it will create it.
      */
     private void checkIfPathExists() {
+        
+        if(!java.nio.file.Files.exists(java.nio.file.Paths.get(scriptsPath))){
+            logger.error("Scripts path does not exist. Please check the completeness of the application.");
+            shutdownManager.initiateShutdown(1);
+        }
         ArrayList<String> paths = new ArrayList<>();
-        paths.add(appRootPath);
-        paths.add(scriptsPath);
         paths.add(infoFilePath);
 
         for(String path : paths){
             if(!java.nio.file.Files.exists(java.nio.file.Paths.get(path))){
                 try {
                     logger.info("Creating path: " + path);
-                    //java.nio.file.Files.createDirectories(java.nio.file.Paths.get(path));
+                    java.nio.file.Files.createDirectories(java.nio.file.Paths.get(path));
                 } catch (Exception e) {
                     logger.error("Error creating path: " + path);
                     e.printStackTrace();
@@ -72,5 +95,7 @@ public class ConfigService {
             }
         }
     }
+
+    
 
 }
--- Simple-Game-Server-Management-Tool/src/main/java/cat/frank/SimpleGameServerManagementTool/serverIp/IPService.java
+++ Simple-Game-Server-Management-Tool/src/main/java/cat/frank/SimpleGameServerManagementTool/serverIp/IPService.java
@@ -4,4 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class IPService {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    public void checkIfIpChanged() {
+        // 
+    }
 }
