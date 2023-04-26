569dfd2524b70379cc4cbefb03014587f0205833
Frank Zhang <fz2907@vt.edu>
2023-04-26 19:28:58 -0400

    Updated start script to make sure start and stop is working

--- scripts/StartSGSMT.sh
+++ scripts/StartSGSMT.sh
@@ -1,79 +1,111 @@
 #!/bin/bash
 
-# Check if the application is already running, if so, do nothing
-if [ "$(ps -ef | grep 'java -jar SGSMT-*.jar' | grep -v grep)" ]
-then
-  echo "The app is already running"
-  exit 0
-fi
+StartingCheck() {
+  # Check if the application is already running, if so, do nothing
+  echo "Check if the app is already running with command ps -ef | grep $jarFile | grep -v grep"
+  if [ "$(ps -ef | grep "$jarFile" | grep -v grep)" ]
+  then
+    echo "The app is already running"
+    exit 0
+  fi
 
-# Warn the user that they have to configure the application.properties file
-echo "Please configure the application.properties file before running this script"
+  # Warn the user that they have to configure the application.properties file
+  echo "Please configure the application.properties file before running this script"
+}
 
-# Stop the app if it is running
-./StopSGSMT.sh
+CreateImportantFolderIfNotExist() {
+  # build the informationFile folder if it does not exist
+  if [ ! -d "$InfoFileFolderPath" ]
+  then
+    mkdir "$InfoFileFolderPath"
+  fi
+}
 
-# Set the path to the root folder of the project
+findTheJarFilePath() {
+  # find the right jar file with version number
+  jarFile=$(find "$SGSMT_Home" -name "SGSMT-*.jar")
+  echo "The jar file is $jarFile"
+}
+
+SetUpLogRotationWhenSudo() {
+  # Setup a log rotation for the log file if it is not set up
+  if [ ! -e "/etc/logrotate.d/SGSMT" ]
+  then
+    echo "Setup log rotation for SGSMT"
+    echo "/tmp/SGSMT.log {
+    rotate 5
+    weekly
+    missingok
+    notifempty
+    compress
+    delaycompress
+    copytruncate
+  }" > /etc/logrotate.d/SGSMT
+  fi
+}
+
+MakeScriptExecutable() {
+  # Before start the application, make all scripts file in the scripts folder executable
+  chmod +x "$SGSMT_Home"/scripts/*.sh
+}
+
+SetupApplicationPropertiesFile() {
+  # Setup the application.properties file name
+  applicationPropertiesFile="$SGSMT_Home/application.properties"
+
+  echo "applicationPropertiesFile is $applicationPropertiesFile"
+}
+
+SetupInfoFile() {
+  # if the PID file exists, remove it
+  if [ -e "$InfoFileFolderPath/SGSMT.pid" ]
+  then
+    echo "Remove the PID file"
+    rm "$InfoFileFolderPath/SGSMT.pid"
+  fi
+  # make a new pid file in the target folder
+  touch "$InfoFileFolderPath/SGSMT.pid"
+
+  # if the jarName file exists, remove it
+  if [ -e "$InfoFileFolderPath/SGSMTJarName.txt" ]
+  then
+    echo "Remove the SGSMTJarName.txt file"
+    rm "$InfoFileFolderPath/SGSMTJarName.txt"
+  fi
+  # make a new jarName file in the target folder
+  touch "$InfoFileFolderPath/SGSMTJarName.txt"
+}
+
+SetupCronJob() {
+  # Check if the cron job is already set up, if it is, do nothing
+  if [ "$(crontab -l | grep 'StartSGSMT.sh')" ]
+  then
+    echo "The cron job StartSGSMT is already set up"
+  else
+    echo "Setup the cron job StartSGSMT"
+    #  evey 30th minute
+    (crontab -l ; echo "*/30 * * * * $ScriptFolderPath/StartSGSMT.sh") | crontab -
+  fi
+}
+
+StartApplication() {
+  # Start the jar application with nohup and the application.properties file, get the return PID and write it to the info file
+  echo "Start the application $jarFile..."
+  echo "$jarFile" > "$InfoFileFolderPath/SGSMTJarName.txt"
+  nohup java -jar $jarFile --spring.config.location="$applicationPropertiesFile" > /tmp/SGSMT.log 2>&1 &
+  echo $! > "$InfoFileFolderPath/SGSMT.pid"
+}
+
+
 source ./StartInit.sh
+findTheJarFilePath
+StartingCheck
+CreateImportantFolderIfNotExist
+#SetUpLogRotationWhenSudo
+MakeScriptExecutable
+SetupApplicationPropertiesFile
+SetupInfoFile
+SetupCronJob
+StartApplication
 
-echo "$SGSMT_Home"
-echo "$ScriptFolderPath"
-echo "$InfoFileFolderPath"
-
-# Go to the root folder of the project
-cd ..
-
-# find the right jar file with version number
-jarFile=$(find . -name "SGSMT-*.jar")
-
-
-# crontab -e
-# */5 * * * * $ScriptFolderPath/StartSGSMT.sh
-
-# check if the cron job is already set up, if it is, do nothing
-if [ "$(crontab -l | grep 'StartSGSMT.sh')" ]
-then
-  echo "The cron job is already set up"
-  exit 0
-fi
-
-# Setup a log rotation for the log file if it is not set up
-if [ ! -e "/etc/logrotate.d/SGSMT" ]
-then
-  echo "Setup log rotation for SGSMT"
-  echo "/tmp/SGSMT.log {
-  rotate 5
-  weekly
-  missingok
-  notifempty
-  compress
-  delaycompress
-  copytruncate
-}" > /etc/logrotate.d/SGSMT
-fi
-
-# Before start the application, make all scripts file in the scripts folder executable
-chmod +x $SGSMT_Home/scripts/*.sh
-
-# use shell script to setup a cron job to run the script every 5 minutes
-# to start the app if it is not running
-(crontab -l ; echo "*/5 * * * * $ScriptFolderPath/StartSGSMT.sh") | crontab -
-
-# Setup the application.properties file name
-applicationPropertiesFile="$SGSMT_Home/application.properties"
-
-echo "applicationPropertiesFile is $applicationPropertiesFile"
-
-# if the PID file exists, remove it
-if [ -e "$InfoFileFolderPath/SGSMT.pid" ]
-then
-  echo "Remove the PID file"
-  rm "$InfoFileFolderPath/SGSMT.pid"
-fi
-
-# make a new pid file in the target folder
-touch "$InfoFileFolderPath/SGSMT.pid"
-
-# Start the jar application with nohup and the application.properties file, get the return PID and write it to the info file
-#nohup java -jar $jarFile --spring.config.location="$applicationPropertiesFile" > /tmp/SGSMT.log 2>&1 &
-#echo $! > "$InfoFileFolderPath/SGSMT.pid"
+# End of the StartSGSMT.sh
--- scripts/StopSGSMT.sh
+++ scripts/StopSGSMT.sh
@@ -1,29 +1,63 @@
 #!/bin/bash
 
-# check if the cron job is already set up, if it is, remove it
-if [ "$(crontab -l | grep 'StartSGSMT.sh')" ]
-then
-  echo "The cron job StartSGSMT is already set up, remove it"
-  crontab -l | grep -v 'StartSGSMT.sh' | crontab -
-else
-  echo "No cron job running"
-fi
+StopCronJob(){
+  # check if the cron job is already set up, if it is, remove it
+  if [ "$(crontab -l | grep 'StartSGSMT.sh')" ]
+  then
+    echo "The cron job StartSGSMT is already set up, remove it"
+    crontab -l | grep -v 'StartSGSMT.sh' | crontab -
+  else
+    echo "No cron job running"
+  fi
+}
 
-# Use PID in the informationFile/SGSMT.pid to stop the app
-if [ -e "$InfoFileFolderPath/SGSMT.pid" ]
-then
-  echo "Stop the app with PID in the informationFile/SGSMT.pid"
-  kill -9 $(cat "$InfoFileFolderPath/SGSMT.pid")
-else
-  echo "No SGSMT PID file found"
-fi
+StopApplication(){
+  # Use Jar name in the informationFile/SGSMTJarName.txt to stop the app
+  if [ -e "$InfoFileFolderPath/SGSMTJarName.txt" ]
+  then
+    echo "Stop the app with Jar name in the $InfoFileFolderPath/SGSMTJarName.txt"
+    # read the Jar name from file
+    JarName=$(cat "$InfoFileFolderPath/SGSMTJarName.txt")
+    # if the application is not running, do nothing
+    if [ ! "$(ps -ef | grep "$JarName" | grep -v grep)" ]
+    then
+      echo "The app is not running"
+      return
+    fi
 
+    # get the PID of the app by the process name
+    App_PID=$(ps -ef | grep "$JarName" | grep -v grep | awk '{print $2}')
+    echo "The PID of $JarName is $App_PID"
+    echo "Run kill -9 $App_PID"
+    kill -9 "$App_PID"
+    # sleep 5 seconds
+    sleep 2
+    # Check if the PID is still running
+    if [ "$(ps -p $App_PID | grep java)" ]
+    then
+      echo "The app is still running"
+    else
+      echo "The app is stopped"
+      # Remove the PID file
+      rm "$InfoFileFolderPath/SGSMT.pid"
+      rm "$InfoFileFolderPath/SGSMTJarName.txt"
+    fi
+  else
+    echo "No SGSMTJarName.txt file found"
+  fi
+}
 
-# Remove the log rotation setup for SGSMT log file
-if [ -e "/etc/logrotate.d/SGSMT" ]
-then
-  echo "Remove the log rotation setup for SGSMT log file"
-  rm /etc/logrotate.d/SGSMT
-else
-  echo "No log rotation setup for SGSMT log file"
-fi
+RemoveLogRotationWhenSudo() {
+  # Remove the log rotation setup for SGSMT log file
+  if [ -e "/etc/logrotate.d/SGSMT" ]
+  then
+    echo "Remove the log rotation setup for SGSMT log file"
+    rm /etc/logrotate.d/SGSMT
+  else
+    echo "No log rotation setup for SGSMT log file"
+  fi
+}
+
+source ./StartInit.sh
+StopCronJob
+StopApplication
