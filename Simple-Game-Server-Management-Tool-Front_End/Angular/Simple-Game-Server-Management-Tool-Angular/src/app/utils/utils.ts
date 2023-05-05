c37155657f09976338f549bb7d82e48b310a97ce
Frank Zhang <fz2907@vt.edu>
2023-05-05 01:40:35 -0400

    Start sys info page

---
+++ Simple-Game-Server-Management-Tool-Front_End/Angular/Simple-Game-Server-Management-Tool-Angular/src/app/utils/utils.ts
@@ -0,0 +1,5 @@
+export const BaseUrl = 'http://localhost:8080';
+export const HomeUrlPath = '/home/';
+export const BackupJobUrlPath = '/buckupJobs/';
+export const JobsUrlPath = '/jobPage/';
+export const JystemInfoUrlPath = '/systemInfo/';
