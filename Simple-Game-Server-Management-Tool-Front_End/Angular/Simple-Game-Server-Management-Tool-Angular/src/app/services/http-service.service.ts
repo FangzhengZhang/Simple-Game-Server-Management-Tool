6b089ab73a4a4f2b495a3aff9e9f2e4f9245f3fb
Frank Zhang <fz2907@vt.edu>
2023-05-05 01:41:53 -0400

    Start sys info page

--- Simple-Game-Server-Management-Tool-Front_End/Angular/Simple-Game-Server-Management-Tool-Angular/src/app/services/http-service.service.ts
+++ Simple-Game-Server-Management-Tool-Front_End/Angular/Simple-Game-Server-Management-Tool-Angular/src/app/services/http-service.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { BaseUrl } from '../utils/static-variables';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type' : 'application/json'
+    })
+  }
+  
+
+  httpGet(requestPath: string): Observable<any> {
+    return this.http.get(BaseUrl + requestPath, this.httpOptions);
+  }
+
+  httpPost(requestPath: string, body: any): Observable<any> {
+    return this.http.post(BaseUrl + requestPath, body, this.httpOptions);
+  }
+
+  httpPut(requestPath: string, body: any): Observable<any> {
+    return this.http.put(BaseUrl + requestPath, body, this.httpOptions);
+  }
+
+  httpDelete(requestPath: string): Observable<any> {
+    return this.http.delete(BaseUrl + requestPath, this.httpOptions);
+  }
+
 }
